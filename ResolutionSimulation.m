00f66587bf55886526a96f877601d09d71380e2c
CadeRPhysics <117719081+CadeRPhysics@users.noreply.github.com>
2026-04-09 10:58:16 +0200

    Add files via upload

---
+++ ResolutionSimulation.m
@@ -0,0 +1,73 @@
+%% Define Simulation Parameters %%
+
+% Define Simulation Grid Parameters
+Nx = 2^10  ; % number of pixels
+dx = 8e-6  ; % pixels size [m]
+Lx = Nx*dx ; % real side length [m]
+[X,Y,Rho,Phi] = sky.MakeGrid(Nx,Nx,dx,dx);
+
+%% Define Beam %%
+
+% General beam parameters
+w0 = 1e-3 ; % Embedded Gaussian beam waist [m]
+
+% LG beam indices
+LR = 2 ; % azimuthal index (OAM) of right-circulalry polarized component
+PR = 0 ; % radial index of right-circulalry polarized component
+LL = 0 ; % azimuthal index (OAM) of left-circulalry polarized component
+PL = 0 ; % radial index of left-circulalry polarized component
+
+% Define Fields
+UR = sky.LG(Rho,Phi,PR,LR,1,w0) ; % right-circulalry polarized field
+UL = sky.LG(Rho,Phi,PL,LL,1,w0) ; % left-circulalry polarized field
+% Normalise so both beams have equal power = 1
+UR = UR/sum(sum(abs(UR).^2));
+UL = UL/sum(sum(abs(UL).^2));
+
+figure(1)
+subplot(2,2,1); imagesc(abs(UR).^2); axis image off; title("I_R")     
+subplot(2,2,2); imagesc(abs(UL).^2); axis image off; title("I_L")
+subplot(2,2,3); imagesc(angle(UR)) ; axis image off; title("arg(U_R)"); clim([-pi pi])
+subplot(2,2,4); imagesc(angle(UL)) ; axis image off; title("arg(U_L)"); clim([-pi pi])
+colormap jet
+
+%% Apply ROI Mask and Compute Stoke Parameters %%
+
+% Defien Resolution Simulation Extent
+min_pix = 10 ;
+max_pix = 40;
+num_pts = max_pix-min_pix+1;
+pix_num = linspace(min_pix,max_pix,num_pts);
+
+% Create HOlding Arrays
+Nsurf = zeros(num_pts,1); 
+Nline = zeros(num_pts,1); 
+for pix_ind = 1:num_pts
+   
+    % Set Resolution Component Modes
+    URresize = imresize(UR,[pix_num(pix_ind) pix_num(pix_ind)]);
+    ULresize = imresize(UL,[pix_num(pix_ind) pix_num(pix_ind)]);
+    
+    % Compute Stokes Parameters
+    [s0,s1,s2,s3] = sky.GetStokesRL(URresize,ULresize);
+    
+    % Plot Stokes
+    figure(1)
+    subplot(2,2,1); imagesc(s0) ; axis image off; clim([ 0 1]); title("s_0")
+    subplot(2,2,2); imagesc(s1) ; axis image off; clim([-1 1]); title("s_1")
+    subplot(2,2,3); imagesc(s2) ; axis image off; clim([-1 1]); title("s_2")
+    subplot(2,2,4); imagesc(s3) ; axis image off; clim([-1 1]); title("s_3")
+    colormap jet
+        
+    % Line Integral Calculation
+    pixcrop = pix_num(pix_ind)/2-1        ; % normally half the simulation space, but can be adjusted
+    BeamSizeInPix = 2*w0/ (dx / pix_num(pix_ind)) ; % approximate size of the beam in pixels to ensure all of it is included in calculation
+    thresh  = 0             ; % threshold for singularity exlcusion
+    [Nline(pix_ind), ~, ~, ~,~] = sky.SkyNumLine(s1,s2,s3,s0,pixcrop,BeamSizeInPix, thresh) ;
+
+    % Surface Integral Calculation
+    [Nsurf(pix_ind), sigz] = sky.SkyNumSurf(s1,s2,s3);
+    
+    disp(pix_ind + ": " + Nsurf(pix_ind) + " , " + Nline(pix_ind))
+
+end
